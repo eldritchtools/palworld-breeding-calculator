e2f1105530f0847e48e95944ad44dd8fc6097388
eldritchtools <eldritch.tools@gmail.com>
2025-09-12 17:30:36 +0800

    Change tooltips to show above the header

--- src/styles.js
+++ src/styles.js
@@ -40,6 +40,6 @@ const selectStyle = {
         flex: 1
     }),
 };
-const tooltipStyle = { outlineStyle: "solid", outlineColor: "#ddd", outlineWidth: "1px", backgroundColor: "#000000", borderRadius: "1rem", zIndex: "999" }
+const tooltipStyle = { outlineStyle: "solid", outlineColor: "#ddd", outlineWidth: "1px", backgroundColor: "#000000", borderRadius: "1rem", zIndex: "9999" }
 
 export { palEquationTextStyle, selectStyle, tooltipStyle };
