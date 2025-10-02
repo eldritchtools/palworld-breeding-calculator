1facb607c9db357565d187a462232d83150bbec8
eldritchtools <eldritch.tools@gmail.com>
2025-10-03 02:15:24 +0800

    Fix text in migrations tab

--- src/tabs/MigrationTab.js
+++ src/tabs/MigrationTab.js
@@ -5,7 +5,7 @@ function MigrationTab() {
             <br /> <br />
             Unfortunately I can't automatically migrate the data from the previous domain due to how it's stored, but I've made the <a href={"https://eldritchtools.github.io/palworld-breeding-calculator-old/"}>old page accessible here</a>.
             <br /> <br />
-            You can export your data from there and import it here using the Settings and Profiles tab.
+            You can export your data from there and import it here using the Profiles tab.
             <br /> <br />
             For now the old version will remain live indefinitely, but it will not receive updates (if any) and may eventually be removed.
         </span>
